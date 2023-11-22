03d897edae3d235c1772de9c277e5fa70c4a8718
Hugo-CASEIRO <151729631+Hugo-CASEIRO@users.noreply.github.com>
2023-11-22 10:53:47 +0100

    Add files via upload

---
+++ Python/helloworld.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ciel')
+def ciel():
+    return render_template('ciel.html')
+
+@app.route('/snir')
+def snir():
+    return render_template('snir.html')
+
+@app.route('/etudes-sup')
+def etudes_sup():
+    return render_template('etudes-sup.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
